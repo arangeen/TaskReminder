0a9b6dff7d810733ede50c7fda4b3b2316e6330f
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-07-09 17:38:39 -0700

    Inserted more documents into database

--- mongodb.js
+++ mongodb.js
@@ -15,9 +15,64 @@ MongoClient.connect(
       return console.log("Unable to connect to database");
     }
     const db = client.db(databaseName);
-    db.collection("users").insertOne({
-      name: "Andrew",
-      age: 27
-    });
+
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Andrew",
+    //     age: 27
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert user");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "Jen",
+    //       age: 28
+    //     },
+    //     {
+    //       name: "Ahmad",
+    //       age: 26
+    //     }
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert documents");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    /**
+     * Goal: Insert 3 tasks into a new tasks collection
+     */
+
+    db.collection("tasks").insertMany(
+      [
+        {
+          description: "Clean the house",
+          completed: true
+        },
+        {
+          description: "Renew Licence",
+          completed: false
+        },
+        {
+          description: "Clean garden",
+          completed: false
+        }
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert tasks");
+        }
+        console.log(result.ops);
+      }
+    );
   }
 );
