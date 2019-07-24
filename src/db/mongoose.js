8395cde81ac6a0b9056686e7c5449ceed42f0e6f
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-07-23 17:28:58 -0700

    Data validation and data sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
@@ -7,17 +8,44 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 
 const User = mongoose.model("User", {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid.");
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    }
   }
 });
 
-/**
- * Goal: Create a model for tasks 
+const me = new User({
+  name: "   Mike   ",
+  email: "MyEMAIL@MEAD.IO  "
+});
 
- */
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch(error => {
+    console.log("Error!", error);
+  });
 
 const Task = mongoose.model("Task", {
   description: {
@@ -28,30 +56,16 @@ const Task = mongoose.model("Task", {
   }
 });
 
-const task = new Task({
-  description: "Learn the Mongoose library",
-  completed: false
-});
-
-task
-  .save()
-  .then(() => {
-    console.log(task);
-  })
-  .catch(error => {
-    console.log(error);
-  });
-
-// // adding new user and saving it to db
-// const me = new User({
-//   name: "Andrew",
-//   age: 27
-// });
-
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch(error => {
-//     console.log("Error!", error);
+// const task = new Task({
+//     description: "Learn the Mongoose library",
+//     completed: false
 //   });
+
+//   task
+//     .save()
+//     .then(() => {
+//       console.log(task);
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
