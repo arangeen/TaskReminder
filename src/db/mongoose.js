82f1274bb786df1ff64ec629975df20bad58cf7c
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-07-29 16:51:53 -0700

    Resource creation endpoints for REST API completed

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,27 +1,7 @@
+// this file connects to the database
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true
 });
-
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  }
-});
-
-/**
- * that was our very first REST API route.
- *  - the route for creating a new user
- *  - we sent data via HTTP request off to the
- *      express server
- *  - using the predefined operation app.post()
- *      we were able to create a user in the database
- */
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 require("./db/mongoose");
 //load user in
 const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 
@@ -16,12 +17,32 @@ app.post("/users", (req, res) => {
   user
     .save()
     .then(() => {
-      res.send(user);
+      res.status(201).send(user);
     })
     .catch(e => {
       res.status(400).send(e);
     });
 });
+
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
+});
+
+/**
+ * Goal: Setup the task creation endpoint
+ *  - creat a separate file for the task model (load it into index.js)
+ *  - create the task creation endpoint (handle success and error )
+ *  - test the endpoint from postman with good and bad data
+ */
 
 app.listen(port, () => {
   console.log("Server is on port " + port);
