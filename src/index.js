a9e4185dd2900adfa9e29dd64ac3c7d033447adb
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-07-30 17:47:31 -0700

    Resource updating endpoint for user added

--- src/index.js
+++ src/index.js
@@ -52,6 +52,35 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
+//updating an exisiting user by id
+app.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  // properties we would like someone to be able to change in mongoDB
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every(update => {
+    return allowedUpdates.includes(update);
+  });
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
 
