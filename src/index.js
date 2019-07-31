f5a0b0cb669beca782448496b5760e74afae502e
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-07-30 18:11:27 -0700

    allow for task updates

--- src/index.js
+++ src/index.js
@@ -12,10 +12,6 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-/**
- * Goal : Refactor user and task routes to use async/await
- */
-
 app.post("/users", async (req, res) => {
   const user = new User(req.body);
 
@@ -112,6 +108,42 @@ app.get("/tasks/:id", async (req, res) => {
     res.send(task);
   } catch (e) {
     res.status(500).send();
+  }
+});
+
+/**
+ * Goal: Allow for task updates
+ *  - setup the route handler
+ *  - send error if unknown updates
+ *  - attempt to update the task
+ *      - handle task not found
+ *      - handle validation errors
+ *      - handle sunccess
+ */
+
+app.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["description", "completed"];
+  const isValidOperation = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates" });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(400).send(e);
   }
 });
 
