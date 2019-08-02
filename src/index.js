ce927d86154aa3237686484c22973ef579908e9c
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-08-01 18:55:55 -0700

    Express Middleware

--- src/index.js
+++ src/index.js
@@ -5,13 +5,40 @@ const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
 const app = express();
-
 // to get app working on heroku
 const port = process.env.PORT || 3000;
+
+// example of middleware function
+// app.use((req, res, next) => {
+//   if (req.method == "GET") {
+//     res.send("Get requests are disabled");
+//   } else {
+//     // next lets express know that we are done with the middleware function
+//     next();
+//   }
+// });
+
+/**
+ * Goal: Setup middleware for maintenance mode
+ *  - register a new middleware function
+ *  - send back a maintenance message with a 503 status code
+ *  - try your requests from the servef and confifrm status/message shows
+ */
+
+app.use((req, res, next) => {
+  res.status(503).send("Site is currently down. Check back soon");
+});
 
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+
+/**
+ *
+ * without middleware: new request -> run route handler
+ *
+ * with middleware: new request => do something => run route handler
+ */
 
 app.listen(port, () => {
   console.log("Server is on port " + port);
